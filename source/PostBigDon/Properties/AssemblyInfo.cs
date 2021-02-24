fbdf05a9cf0bad927961cbb006554f0b11700a91
6K6666 <68512888+6K6666@users.noreply.github.com>
2021-02-24 00:51:07 -0700

    Add files via upload

---
+++ source/PostBigDon/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("PostBigDon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PostBigDon")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("833537bd-9360-4447-b21f-963af6487747")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
